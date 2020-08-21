ca0d31587d438d6e94c47a7f6b7a35d2a6197d6b
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-08-21 13:28:08 +0530

    Change button color and clickable on user valid input in input section

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+      buttonClassName: "loginsignupbutton",
+    };
+  }
+
+  handleInput(e) {
+    this.setState({
+      username: e.target.value,
+      password: e.target.value,
+    });
+    if (this.state.username.length > 1 && this.state.password.length > 6) {
+      this.setState({
+        buttonClassName: "loginsignupbuttonactive"
+      });
+    }
+  }
+
   render() {
+    const { buttonClassName } = this.state;
     return (
       <div className="loginsection">
         <div className="instalogo">
@@ -16,22 +39,32 @@ class Login extends Component {
             type="text"
             className="logintextbox"
             placeholder="Phone number, username, or email"
+            onChange={(e) => this.handleInput(e)}
           />
           <br />
           <input
             type="password"
             className="logintextbox "
             placeholder="Password"
+            onChange={(e) => this.handleInput(e)}
           />
           <br />
-          <input type="button" className="loginsignupbutton" value="Log In" />
+          <input
+            type="button"
+            className={buttonClassName}
+            value="Log In"
+          />
         </form>
         <div className="loginsignupor">
           <div className="or">OR</div>
         </div>
         <button className="loginwithfacebook">
           <span className="loginwithfacebookicon">
-            <FontAwesomeIcon size="lg" color="#3C5998" icon={faFacebookSquare} />
+            <FontAwesomeIcon
+              size="lg"
+              color="#3C5998"
+              icon={faFacebookSquare}
+            />
           </span>
           {/* eslint-disable-next-line */}
           <span className="loginwithfacebooktext" href="#">
